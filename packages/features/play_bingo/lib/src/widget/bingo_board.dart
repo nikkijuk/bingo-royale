0358d5c1a45f687f1dfa5cfc0e0e3bb5ad6f9372
Jukka Nikki <jukka.nikki@kolumbus.fi>
2025-04-27 22:35:11 +0200

    moved cell to own ui component

--- packages/features/play_bingo/lib/src/widget/bingo_board.dart
+++ packages/features/play_bingo/lib/src/widget/bingo_board.dart
@@ -4,14 +4,6 @@ import 'package:play_bingo/src/bloc/game_bloc.dart';
 import 'package:play_bingo/src/bloc/game_event.dart';
 import 'package:play_bingo/src/bloc/game_state.dart';
 
-const selectedCell = TextStyle(
-  color: Colors.red,
-);
-
-const freeCell = TextStyle(
-  color: Colors.green,
-);
-
 class BingoBoard extends StatelessWidget {
   const BingoBoard({
     super.key,
@@ -19,35 +11,82 @@ class BingoBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GameBloc, GameState>(builder: (context, state) {
-      final bloc = context.read<GameBloc>();
-      return Container(
-        width: 500,
-        height: 500,
-        padding: const EdgeInsets.all(10),
-        child:
-        GridView.count(
-        crossAxisCount: state.width,
-        children: state
-            .cells
-            .map((cell) => InkWell(
-                  onTap: cell.selected
-                      ? () => bloc.add(Unselected(cell.label))
-                      : () => bloc.add(Selected(cell.label)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        cell.label,
-                        textAlign: TextAlign.center,
-                        style: cell.selected ? selectedCell : freeCell,
-                      ),
-                    ],
+    return BlocBuilder<GameBloc, GameState>(
+      builder: (context, state) {
+        final bloc = context.read<GameBloc>();
+
+        return Container(
+          width: 500,
+          height: 500,
+          padding: const EdgeInsets.all(10),
+          child: GridView.count(
+            crossAxisCount: state.width,
+            children: state.cells
+                .map(
+                  (cell) => CellWidget(
+                    cell: cell,
+                    onSelect: (label) => bloc.add(Selected(label)),
+                    onUnselect: (label) => bloc.add(Unselected(label)),
                   ),
-                ),)
-            .toList(),
-      ),
-      );
-    },);
+                )
+                .toList(),
+          ),
+        );
+      },
+    );
   }
 }
+
+/// cell widget that contains inkwell
+/// inkwell is used to detect tap
+class CellWidget extends StatelessWidget {
+  const CellWidget({
+    required this.cell,
+    required this.onSelect,
+    required this.onUnselect,
+    super.key,
+  });
+
+  final Cell cell;
+
+  final PressCell onSelect;
+  final PressCell onUnselect;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: cell.selected
+          ? () => onUnselect(cell.label)
+          : () => onSelect(cell.label),
+      child: Container(
+        color: cell.selected ? selectedBgColor : freeBgColor,
+        alignment: Alignment.center,
+        child: Text(
+          cell.label,
+          textAlign: TextAlign.center,
+          style: cell.selected ? selectedCell : freeCell,
+        ),
+      ),
+    );
+  }
+}
+
+/// Button press handler for cell of bingo board
+/// Takes label of cell as parameter
+typedef PressCell = void Function(String);
+
+/// Colors for bingo board
+const selectedBgColor = Colors.yellow;
+
+/// Colors for bingo board
+const freeBgColor = Colors.white;
+
+/// Text style for selected cell
+const selectedCell = TextStyle(
+  color: Colors.red,
+);
+
+/// Text style for free cell
+const freeCell = TextStyle(
+  color: Colors.green,
+);
