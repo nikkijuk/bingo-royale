1e61a7cd5cfe8a145bf97386fa89393b8fe6f3df
Jukka Nikki <jukka.nikki@kolumbus.fi>
2025-04-28 08:31:45 +0200

    refactored cell to own file and decoupled it from state

--- packages/features/play_bingo/lib/src/widget/bingo_board.dart
+++ packages/features/play_bingo/lib/src/widget/bingo_board.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:play_bingo/src/bloc/game_bloc.dart';
 import 'package:play_bingo/src/bloc/game_event.dart';
 import 'package:play_bingo/src/bloc/game_state.dart';
+import 'package:play_bingo/src/widget/bingo_board_cell.dart';
 
 class BingoBoard extends StatelessWidget {
   const BingoBoard({
@@ -23,8 +24,9 @@ class BingoBoard extends StatelessWidget {
             crossAxisCount: state.width,
             children: state.cells
                 .map(
-                  (cell) => CellWidget(
-                    cell: cell,
+                  (cell) => BingoBoardCell(
+                    label: cell.label,
+                    isSelected: cell.selected,
                     onSelect: (label) => bloc.add(Selected(label)),
                     onUnselect: (label) => bloc.add(Unselected(label)),
                   ),
@@ -36,57 +38,3 @@ class BingoBoard extends StatelessWidget {
     );
   }
 }
-
-/// cell widget that contains inkwell
-/// inkwell is used to detect tap
-class CellWidget extends StatelessWidget {
-  const CellWidget({
-    required this.cell,
-    required this.onSelect,
-    required this.onUnselect,
-    super.key,
-  });
-
-  final Cell cell;
-
-  final PressCell onSelect;
-  final PressCell onUnselect;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: cell.selected
-          ? () => onUnselect(cell.label)
-          : () => onSelect(cell.label),
-      child: Container(
-        color: cell.selected ? selectedBgColor : freeBgColor,
-        alignment: Alignment.center,
-        child: Text(
-          cell.label,
-          textAlign: TextAlign.center,
-          style: cell.selected ? selectedCell : freeCell,
-        ),
-      ),
-    );
-  }
-}
-
-/// Button press handler for cell of bingo board
-/// Takes label of cell as parameter
-typedef PressCell = void Function(String);
-
-/// Colors for bingo board
-const selectedBgColor = Colors.yellow;
-
-/// Colors for bingo board
-const freeBgColor = Colors.white;
-
-/// Text style for selected cell
-const selectedCell = TextStyle(
-  color: Colors.red,
-);
-
-/// Text style for free cell
-const freeCell = TextStyle(
-  color: Colors.green,
-);
--- packages/features/play_bingo/lib/src/widget/bingo_board_cell.dart
+++ packages/features/play_bingo/lib/src/widget/bingo_board_cell.dart
@@ -1,13 +1,57 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class BingoBoardCell {
-  BingoBoardCell({
-    required this.icon,
+/// cell widget that contains inkwell
+/// inkwell is used to detect tap
+class BingoBoardCell extends StatelessWidget {
+  const BingoBoardCell({
     required this.label,
-    required this.onTap,
+    required this.isSelected,
+    required this.onSelect,
+    required this.onUnselect,
+    super.key,
   });
 
-  final IconData icon;
   final String label;
-  final VoidCallback onTap;
+  final bool isSelected;
+
+  final PressCell onSelect;
+  final PressCell onUnselect;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isSelected
+          ? () => onUnselect(label)
+          : () => onSelect(label),
+      child: Container(
+        color: isSelected ? selectedBgColor : freeBgColor,
+        alignment: Alignment.center,
+        child: Text(
+          label,
+          textAlign: TextAlign.center,
+          style: isSelected ? selectedCell : freeCell,
+        ),
+      ),
+    );
+  }
 }
+
+/// Button press handler for cell of bingo board
+/// Takes label of cell as parameter
+typedef PressCell = void Function(String);
+
+/// Colors for bingo board
+const selectedBgColor = Colors.yellow;
+
+/// Colors for bingo board
+const freeBgColor = Colors.white;
+
+/// Text style for selected cell
+const selectedCell = TextStyle(
+  color: Colors.red,
+);
+
+/// Text style for free cell
+const freeCell = TextStyle(
+  color: Colors.green,
+);
